90b874b91658bef610767035be2d36add7e297bf
LemoyneAllaire <maxigrav@hotmail.com>
2024-02-20 22:08:03 -0500

    Update can library

---
+++ canBusV3/src/main.cpp
@@ -0,0 +1,18 @@
+#include <Arduino.h>
+#include "driver/can.h"
+#include "can_lib.h"
+class can_lib can_lib;
+
+void setup() {
+  can_lib.can_init(GPIO_NUM_4, GPIO_NUM_5);
+}
+
+//==================================================================================//
+
+void loop() {
+  uint8_t data[2];
+  data[0] = 0xBE;
+  data[1] = 0xEF;
+  can_lib.can_write_msg(0x069, 2, data);
+  delay(1000);
+}
