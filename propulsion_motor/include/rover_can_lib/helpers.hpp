475344601cd9e7f3dba11db9cb56f83d36e4b149
pmichaud29 <micp1402@usherbrooke.ca>
2024-03-12 01:50:29 -0400

    Added error message to master + better header + added enums

--- propulsion_motor/include/rover_can_lib/helpers.hpp
+++ propulsion_motor/include/rover_can_lib/helpers.hpp
@@ -3,6 +3,7 @@
 
 #include <type_traits>
 #include "rover_can_lib/rover_can_lib.hpp"
+#include "rover_can_lib/msgs/error_state.hpp"
 
 #if defined(ESP32)
 #include "driver/twai.h"
@@ -17,17 +18,11 @@ namespace RoverCanLib::Helpers
     // nodes, it's used internally to say that theres no more message in the rx
     // queue
 #if defined(ESP32)
-    inline twai_message_t getErrorIdMsg(void)
-    {
-        twai_message_t msg;
-        msg.identifier = 0u;
-
-        return msg;
-    }
+    extern twai_message_t getErrorIdMsg(void);
 #endif // defined(ESP32)
 
     template <typename COPY_TYPE, typename UNION_TYPE>
-    void canMsgToStruct(IN const void *src_, OUT COPY_TYPE *dest_)
+    void canMsgToStruct(IN const twai_message_t *msg_, OUT COPY_TYPE *dest_)
         requires(std::is_same_v<COPY_TYPE, float> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::FloatUnion>) ||
                 (std::is_same_v<COPY_TYPE, bool> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::BoolUnion>) ||
                 (std::is_same_v<COPY_TYPE, uint8_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Uint8_tUnion>) ||
@@ -37,15 +32,59 @@ namespace RoverCanLib::Helpers
                 (std::is_same_v<COPY_TYPE, int16_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Int16_tUnion>) ||
                 (std::is_same_v<COPY_TYPE, int32_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Int32_tUnion>)
     {
+        if (msg_->data_length_code < ((uint8_t)Constant::eDataIndex::START_OF_DATA + sizeof(COPY_TYPE)))
+        {
+            LOG(WARN, "Wrong length for data type, dropping msg");
+            return;
+        }
+
         UNION_TYPE copyUnion;
 
         for (uint8_t i = 0; i < sizeof(copyUnion); i++)
         {
-            copyUnion.dataBytes[i] = *((uint8_t *)src_ + i);
+            copyUnion.dataBytes[i] = *((uint8_t *)msg_->data + (uint8_t)Constant::eDataIndex::START_OF_DATA + i);
         }
 
         *dest_ = copyUnion.data;
     }
+
+    template <typename COPY_TYPE, typename UNION_TYPE>
+    void structToCanMsg(IN const COPY_TYPE *structMember_, OUT twai_message_t *msg_)
+        requires(std::is_same_v<COPY_TYPE, float> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::FloatUnion>) ||
+                (std::is_same_v<COPY_TYPE, bool> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::BoolUnion>) ||
+                (std::is_same_v<COPY_TYPE, uint8_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Uint8_tUnion>) ||
+                (std::is_same_v<COPY_TYPE, uint16_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Uint16_tUnion>) ||
+                (std::is_same_v<COPY_TYPE, uint32_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Uint32_tUnion>) ||
+                (std::is_same_v<COPY_TYPE, int8_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Int8_tUnion>) ||
+                (std::is_same_v<COPY_TYPE, int16_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Int16_tUnion>) ||
+                (std::is_same_v<COPY_TYPE, int32_t> && std::is_same_v<UNION_TYPE, RoverCanLib::UnionDefinition::Int32_tUnion>)
+    {
+        msg_->data_length_code = ((uint8_t)Constant::eDataIndex::START_OF_DATA + sizeof(COPY_TYPE));
+
+        UNION_TYPE copyUnion;
+        copyUnion.data = *structMember_;
+
+        for (uint8_t i = 0; i < sizeof(copyUnion); i++)
+        {
+            *((uint8_t *)msg_->data + (uint8_t)Constant::eDataIndex::START_OF_DATA + i) = copyUnion.dataBytes[i];
+        }
+    }
+
+    // void sendErrorMsg(RoverCanLib::Constant::eInternalErrorCode errCode, RoverCanLib::CanBusManager* canManager_)
+    // {
+//     if (errCode == Constant::eInternalErrorCode::WARNING)
+    //     {
+    //         canManager_->setWarningFlag();
+    //         // _errorStateMsg.data.warning = true;
+    //         // this->sendMsg(&_errorStateMsg, true);
+    //     }
+
+    //     if (errCode == Constant::eInternalErrorCode::WARNING)
+    //     {
+    //         // _errorStateMsg.data.error = true;
+    //         // this->sendMsg(&_errorStateMsg, true);
+    //     }
+    // }
 }
 
 #endif // __HELPERS_HPP__
