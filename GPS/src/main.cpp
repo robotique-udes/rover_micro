af53ff3920c773bab72127e8ad35bbb416309c97
Aude Regnault <rega2002@usherbrooke.ca>
2024-04-29 22:26:04 -0400

    formatted

--- GPS/src/main.cpp
+++ GPS/src/main.cpp
@@ -23,9 +23,7 @@ void setup()
   Serial.begin(115200);
   Serial1.begin(9600, SERIAL_8N1, RX1PIN, TX1PIN);
 
- 
-
-  for(EVER)
+  for (EVER)
   {
     char bufferGpsReceive[BUFFER_SIZE] = {0};
     uint8_t dataSize = Serial1.readBytesUntil('\n', bufferGpsReceive, BUFFER_SIZE - 1);
@@ -35,14 +33,14 @@ void setup()
       // Good Received data: $GNGGA,201427.00,4522.65273,N,07155.50339,W,2,12,0.71,282.0,M,-31.5,M,,0000*78
       bufferGpsReceive[dataSize] = '\0';
       const char *pGpsData = bufferGpsReceive;
-      char gpsDataTest[BUFFER_SIZE] = {'$','G','N','G','G','A',',','2','0','1','4','2','7','.','0','0',',','4','5','2','2','.','6','5','2','7','3',',','N',',','0','7','1','5','5','.','5','0','3','3','9',',','W',',','2',',','1','2',',','0','.','7','1',',','2','8','2','.','0',',','M',',','-','3','1','.','5',',','M',',',',','0','0','0','0','*','7','8','\0'};
+      char gpsDataTest[BUFFER_SIZE] = {'$', 'G', 'N', 'G', 'G', 'A', ',', '2', '0', '1', '4', '2', '7', '.', '0', '0', ',', '4', '5', '2', '2', '.', '6', '5', '2', '7', '3', ',', 'N', ',', '0', '7', '1', '5', '5', '.', '5', '0', '3', '3', '9', ',', 'W', ',', '2', ',', '1', '2', ',', '0', '.', '7', '1', ',', '2', '8', '2', '.', '0', ',', 'M', ',', '-', '3', '1', '.', '5', ',', 'M', ',', ',', '0', '0', '0', '0', '*', '7', '8', '\0'};
       const char *pGpsDataTest = gpsDataTest;
       splitData(pGpsData);
     }
   }
 }
 
-void loop(){}
+void loop() {}
 
 int splitData(const char *pGpsData)
 {
@@ -74,13 +72,14 @@ int splitData(const char *pGpsData)
       }
     }
   }
-  
+
   char compareMsgType[] = "$GNGGA";
   if (strcmp(data[0], compareMsgType) == 0)
   {
     LOG(INFO, "Good format of received data: %s", pGpsData);
   }
-  else{
+  else
+  {
     valide = false;
   }
 
@@ -98,7 +97,8 @@ int splitData(const char *pGpsData)
       latitude = getLatitude(data[2], data[3]);
       LOG(INFO, "Latitude: %f", latitude);
     }
-    else{
+    else
+    {
       valide = false;
       LOG(WARN, "Empty messages");
     }
@@ -108,7 +108,8 @@ int splitData(const char *pGpsData)
       longitude = getLongitude(data[4], data[5]);
       LOG(INFO, "Longitude: %f", longitude);
     }
-    else{
+    else
+    {
       valide = false;
       LOG(WARN, "Empty messages");
     }
@@ -133,18 +134,18 @@ float getLatitude(char latData[], char latSign[])
       temp = latData[i] - '0';
       if (iDeg >= 0)
       {
-        
-        allDegrees += temp * pow(10,iDeg);
+
+        allDegrees += temp * pow(10, iDeg);
         iDeg--;
       }
       else if (iDeg == -1)
       {
-        allMinutes += temp * pow(10,iMin);
+        allMinutes += temp * pow(10, iMin);
         iMin--;
       }
     }
   }
-  float latitude = allDegrees + (allMinutes/60);
+  float latitude = allDegrees + (allMinutes / 60);
   if (latSign[0] == 'S')
   {
     latitude *= -1;
@@ -168,19 +169,19 @@ float getLongitude(char longData[], char longSign[])
       temp = longData[i] - '0';
       if (iDeg >= 0)
       {
-        
-        allDegrees += temp * pow(10,iDeg);
+
+        allDegrees += temp * pow(10, iDeg);
         iDeg--;
       }
       else if (iDeg == -1)
-      {        
-        allMinutes += temp * pow(10,iMin);
+      {
+        allMinutes += temp * pow(10, iMin);
         iMin--;
       }
     }
   }
 
-  float longitude = allDegrees + (allMinutes/60);
+  float longitude = allDegrees + (allMinutes / 60);
   if (longSign[0] == 'W')
   {
     longitude *= -1;
