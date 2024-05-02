e8bf0da47ac92e9f1dc68d185ca60892549fcbe1
pmichaud29 <micp1402@usherbrooke.ca>
2024-05-02 02:51:45 -0400

    prop motor low level

--- lib/lib_rover/include/actuators/talon_srx.hpp
+++ lib/lib_rover/include/actuators/talon_srx.hpp
@@ -10,23 +10,22 @@
 #include "actuators/motor_driver.hpp"
 #include "rover_helpers/log.hpp"
 
-namespace TalonSrxConstant
+class TalonSrx : public MotorDriver
 {
-    constexpr float SIGNAL_FULL_STOP_MS = 1500.0f; // Middle of deadband
-    constexpr float SIGNAL_FULL_REVERSE_MS = 1000.0f;
-    constexpr float SIGNAL_MIN_REVERSE_MS = 1480.0f;
-    constexpr float SIGNAL_FULL_FORWARD_MS = 2000.0f;
-    constexpr float SIGNAL_MIN_FORWARD_MS = 1520.0f;
+private:
+    static constexpr float PROTECTION_MAX_VOLTAGE = 12.0f; // In volts
+    static constexpr float SIGNAL_FULL_STOP_MS = 1500.0f;  // Middle of deadband
+    static constexpr float SIGNAL_FULL_REVERSE_MS = 1000.0f;
+    static constexpr float SIGNAL_MIN_REVERSE_MS = 1480.0f;
+    static constexpr float SIGNAL_FULL_FORWARD_MS = 2000.0f;
+    static constexpr float SIGNAL_MIN_FORWARD_MS = 1520.0f;
 
-    constexpr uint32_t MS_TO_DUTY(uint32_t freq_, float microSeconds_)
+    static constexpr uint32_t MS_TO_DUTY(uint32_t freq_, float microSeconds_)
     {
         float period = 1.0f / static_cast<float>(freq_);
         return static_cast<uint32_t>(round(((microSeconds_ / 1'000'000.f) / period) * 1048576.0f));
     }
-}
 
-class TalonSrx : public MotorDriver
-{
 public:
     // Try using the same timer for all same frequency signal. Don't use the same channel
     TalonSrx(gpio_num_t pinPWM_, ledc_timer_t timerNumber_ = LEDC_TIMER_0, ledc_channel_t channelNumber_ = LEDC_CHANNEL_0)
@@ -41,8 +40,10 @@ public:
         ledc_stop(LEDC_LOW_SPEED_MODE, _channel, 0u);
     }
 
-    void init(float microseconds_ = TalonSrxConstant::SIGNAL_FULL_STOP_MS, float signalFrequencyHz_ = 50.0F)
+    void init(float signalFrequencyHz_ = 50.0f)
     {
+        pinMode(_pinPWM, OUTPUT);
+
         _freqSignal = (uint32_t)round(signalFrequencyHz_);
         ledc_timer_config_t ledc_timer;
         ledc_timer.speed_mode = LEDC_LOW_SPEED_MODE;
@@ -58,88 +59,121 @@ public:
         ledc_channel.timer_sel = _timer;
         ledc_channel.intr_type = LEDC_INTR_DISABLE;
         ledc_channel.gpio_num = _pinPWM;
-        ledc_channel.duty = TalonSrxConstant::MS_TO_DUTY(_freqSignal, microseconds_);
+        ledc_channel.duty = MS_TO_DUTY(_freqSignal, SIGNAL_FULL_STOP_MS);
         ledc_channel.hpoint = 0;
         ledc_channel_config(&ledc_channel);
 
-        ASSERT(_freqSignal == 0u || _freqSignal < 10u || _freqSignal > 200u, "Invalid signal frequency for TalonSrx")
+        ASSERT(_freqSignal < 10u || _freqSignal > 200u, "Invalid signal frequency for TalonSrx")
+
+        this->setMaxVoltage(25.2f, PROTECTION_MAX_VOLTAGE);
 
         ledc_set_freq(LEDC_LOW_SPEED_MODE, _timer, _freqSignal);
-        this->writeMicroseconds(microseconds_);
+        this->writeMicroseconds(SIGNAL_FULL_STOP_MS);
     }
 
-    // -100 to 100 for speed
-    void setSpd(float spd)
+    // -100.0 to 100.0 for speed
+    void setSpeed(float speed_)
     {
-        if (spd < -_maxSpeed || spd > _maxSpeed)
+        if (speed_ < -_protectionSpeed || speed_ > _protectionSpeed)
         {
-            LOG(WARN, "Speed value exceed max power: %f", spd);
-            _spd = (spd < -_maxSpeed) ? -_maxSpeed : _maxSpeed;
+            LOG(WARN, "Speed value exceed max power: %f", speed_);
+            _speed = (speed_ < -_protectionSpeed) ? -_protectionSpeed : _protectionSpeed;
         }
         else
         {
-            _spd = spd;
+            _speed = speed_;
         }
-        writeMicroseconds(MAP(_spd, -100.0f, 100.0f, 1000.0f, 2000.0f));
+
+        if (_speed == 0.0f)
+        {
+            this->stop();
+        }
+        else if (_speed < 0.0f)
+        {
+            writeMicroseconds(MAP(_speed, -100.0f, 0.0f, SIGNAL_FULL_REVERSE_MS, SIGNAL_MIN_REVERSE_MS));
+        }
+        else if (_speed > 0.0f)
+        {
+            writeMicroseconds(MAP(_speed, 0.0f, 100.0f, SIGNAL_MIN_FORWARD_MS, SIGNAL_FULL_FORWARD_MS));
+        }
     }
 
-    void setSpd(float spd, bool override_)
+    void enable(void)
     {
-        if (override_ == true)
-        {
-            if (spd < -_maxSpeed || spd > _maxSpeed)
-            {
-                LOG(WARN, "Speed value exceed max power: %f", spd);
-            }
-            writeMicroseconds(MAP(spd, -100.0f, 100.0f, 1000.0f, 2000.0f));
-        }
-        else
-        {
-            LOG(WARN, "Cant't override max speed: %f", spd);
-        }
+        LOG(INFO, "Enabling not supported on talonSRX");
     }
 
-    void enableMot(void) { /*code to enable the motor if applied.*/ }
-    void disableMot(void)
+    void disable(void)
     {
         stop();
     }
-    void resetMot(void) { /*code to reset the motor if applied.*/ }
+
+    void reset(void)
+    {
+        LOG(INFO, "Resetting not supported on talonSRX");
+    }
 
     // Return true if the motor is moving.
     bool isMoving(void)
     {
-        if (_spd != 0)
+        if (_speed != 0)
+        {
             return true;
+        }
         else
+        {
             return false;
+        }
     }
 
     // Set max dutycycle from 0-100% to limit the output power.
-    void setMaxSpeed(uint8_t spd)
+    void setMaxVoltage(float alimVoltage, float maxVoltage, bool removeOvervoltageSecurity = false)
     {
-        _maxSpeed = spd;
-        LOG(INFO, "Max speed set at : %i", spd);
+        if (alimVoltage < 0.0f || maxVoltage < 0)
+        {
+            LOG(WARN, "Wrong input parameters: can't have negative voltages. New value won't be applied...");
+            return;
+        }
+
+        if (maxVoltage > alimVoltage)
+        {
+            LOG(WARN, "Wrong input parameters: can't set higher max voltage than alim voltage. New value won't be applied...");
+            return;
+        }
+
+        float newMax = 0.0f;
+        if (removeOvervoltageSecurity)
+        {
+            LOG(WARN, "Removing the overvoltage security will permanently damage the motor if you don't know what you're doing");
+            newMax = maxVoltage;
+        }
+        else
+        {
+            newMax = constrain(maxVoltage, 0.0f, PROTECTION_MAX_VOLTAGE);
+        }
+
+        _protectionSpeed = MAP(newMax, 0.0f, alimVoltage, 0.0f, 100.0f);
+        LOG(INFO, "New max speed set at : %f which should correspond to approx %f V", _protectionSpeed, newMax);
     }
 
 private:
     void writeMicroseconds(float microseconds_)
     {
-        ledc_set_duty(LEDC_LOW_SPEED_MODE, _channel, TalonSrxConstant::MS_TO_DUTY(_freqSignal, microseconds_));
+        ledc_set_duty(LEDC_LOW_SPEED_MODE, _channel, MS_TO_DUTY(_freqSignal, microseconds_));
         ledc_update_duty(LEDC_LOW_SPEED_MODE, _channel);
     }
 
-    void stop()
+    void stop(void)
     {
-        writeMicroseconds(TalonSrxConstant::SIGNAL_FULL_STOP_MS);
+        writeMicroseconds(SIGNAL_FULL_STOP_MS);
     }
 
-    uint8_t _maxSpeed = 30u;
+    uint8_t _protectionSpeed = 0u;
     gpio_num_t _pinPWM;
     ledc_timer_t _timer;
     ledc_channel_t _channel;
     uint32_t _freqSignal;
-    float _spd;
+    float _speed;
 };
 
 #endif // !defined(ESP32)
--- propulsion_motor/src/propulsion_motor.cpp
+++ propulsion_motor/src/propulsion_motor.cpp
@@ -10,15 +10,6 @@
 #include "actuators/talon_srx.hpp"
 #include "actuators/motor_driver.hpp"
 
-#define PMW_MOT GPIO_NUM_26
-#define BTN_1 GPIO_NUM_34
-#define BTN_2 GPIO_NUM_39
-#define BTN_3 GPIO_NUM_36
-
-#define LED_1 GPIO_NUM_32
-#define LED_2 GPIO_NUM_33
-#define LED_3 GPIO_NUM_25
-
 void canCB(RoverCanLib::CanBusManager *canBusManager_, const twai_message_t *msg_);
 void parseDeviceIdMsg(RoverCanLib::CanBusManager *canBusManager_, const twai_message_t *msg_);
 
@@ -30,60 +21,44 @@ void setup()
     Serial.begin(115200);
 
     pinMode(PMW_MOT, OUTPUT);
-    pinMode(BTN_1, INPUT);
-    pinMode(BTN_2, INPUT);
-    pinMode(BTN_3, INPUT);
-    pinMode(LED_1, OUTPUT);
-    pinMode(LED_2, OUTPUT);
-    pinMode(LED_3, OUTPUT);
 
     TalonSrx talonDrive(PMW_MOT, LEDC_TIMER_0, LEDC_CHANNEL_0);
-    talonDrive.init(1500.0f);
+    talonDrive.init();
 
-    RoverCanLib::CanBusManager canBus(DEVICE_ID, GPIO_NUM_4, GPIO_NUM_18, canCB, true, (gpio_num_t)LED_BUILTIN);
+    RoverCanLib::CanBusManager canBus(DEVICE_ID, CAN_TX, CAN_RX, canCB, true, (gpio_num_t)LED_BUILTIN);
     canBus.init();
 
-    Serial.printf("Init done, starting Loop!\n");
+    LOG(INFO, "Init done, starting Loop!\n");
     RoverHelpers::Timer<unsigned long, millis> timerFeedback(100); // 10 Hz
-    RoverHelpers::Timer<unsigned long, millis> timer500(100);
-    float testSpeed = 0;
-    // talonDrive.setMaxSpeed(64); // enlever par sécurité
 
     for (;;)
     {
-        if (timer500.isDone())
+        canBus.update();
+
+        if (canBus.isOk() && msgPropCmd.data.enable)
         {
-            digitalWrite(LED_1, !digitalRead(LED_1));
-            talonDrive.setSpd(30);
+            talonDrive.setSpeed(msgPropCmd.data.targetSpeed);
+        }
+        else
+        {
+            talonDrive.disable();
         }
 
-        // talonDrive.writeMicroseconds(MAP(testSpeed, -100.0f, 100.0f, 1000.0f, 2000.0f));
-        // canBus.update();
+        if (timerFeedback.isDone())
+        {
+            if (!msgPropCmd.data.closeLoop)
+            {
+                msgPropStatus.data.currentSpeed = msgPropCmd.data.targetSpeed;
+            }
+            else
+            {
+                LOG(ERROR, "Close loop not implemented yet");
+                canBus.sendErrorCode(RoverCanLib::Constant::eInternalErrorCode::ERROR);
+                msgPropStatus.data.currentSpeed = -69.0f;
+            }
 
-        // if (canBus.isOk() && msgPropCmd.data.enable)
-        // {
-        // talonDrive.writeMicroseconds(MAP(msgPropCmd.data.targetSpeed, -100.0f, 100.0f, 1000.0f, 2000.0f));
-        // }
-        // else
-        // {
-        //     motorDrive.writeMicroseconds(TalonSrxConstant::SIGNAL_FULL_STOP_MS);
-        // }
-
-        // if (timerFeedback.isDone())
-        // {
-        //     if (!msgPropCmd.data.closeLoop)
-        //     {
-        //         msgPropStatus.data.currentSpeed = msgPropCmd.data.targetSpeed;
-        //     }
-        //     else
-        //     {
-        //         LOG(ERROR, "Close loop not implemented yet");
-        //         canBus.sendErrorCode(RoverCanLib::Constant::eInternalErrorCode::ERROR);
-        //         msgPropStatus.data.currentSpeed = -69.0f;
-        //     }
-
-        //     canBus.sendMsg(&msgPropStatus);
-        // }
+            canBus.sendMsg(&msgPropStatus);
+        }
     }
 }
 
