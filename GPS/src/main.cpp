14b0c9a56b59b6ed5c18f6f628faa85d41929b6f
pmichaud29 <micp1402@usherbrooke.ca>
2024-05-03 21:32:51 -0400

    added can test

--- GPS/src/main.cpp
+++ GPS/src/main.cpp
@@ -1,6 +1,14 @@
 #include <Arduino.h>
 #include "math.h"
+
+#include "rover_can_lib/can_bus_manager.hpp"
+// #include "rover_can_lib/msgs/"
+
 #include "rover_helpers/helpers.hpp"
+
+#define DEVICE_ID (uint16_t)RoverCanLib::Constant::eDeviceId::FRONTLEFT_MOTOR
+#define CAN_TX GPIO_NUM_47
+#define CAN_RX GPIO_NUM_48
 
 #define RX1PIN GPIO_NUM_12
 #define TX1PIN GPIO_NUM_13
@@ -9,182 +17,209 @@
 #define NB_ELEMENTS 20
 #define SIZE_ELEMENTS 255
 
-float getLatitude(char latData[SIZE_ELEMENTS], char latSign[SIZE_ELEMENTS]);
-void splitData(const char pGpsData[BUFFER_SIZE]);
-float getLongitude(char longData[SIZE_ELEMENTS], char longSign[SIZE_ELEMENTS]);
+float getLatitude(char latData_[SIZE_ELEMENTS], char latSign_[SIZE_ELEMENTS]);
+void splitData(const char gpsData_[BUFFER_SIZE]);
+float getLongitude(char longData_[SIZE_ELEMENTS], char longSign_[SIZE_ELEMENTS]);
+void canCB(RoverCanLib::CanBusManager *canBusManager_, const twai_message_t *msg_);
 
-float latitude;
-float longitude;
-uint8_t fixType;
+float g_latitude;
+float g_longitude;
+uint8_t g_fixType;
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial1.begin(9600, SERIAL_8N1, RX1PIN, TX1PIN);
+    Serial.begin(115200);
+    Serial1.begin(9600, SERIAL_8N1, RX1PIN, TX1PIN);
 
-  for (EVER)
-  {
-    char bufferGpsReceive[BUFFER_SIZE] = {0};
-    uint8_t dataSize = Serial1.readBytesUntil('\n', bufferGpsReceive, BUFFER_SIZE);
+    RoverCanLib::CanBusManager canBus(DEVICE_ID, CAN_TX, CAN_RX, canCB, true, (gpio_num_t)LED_BUILTIN);
+    canBus.init();
 
-    if (dataSize > 0)
+    for (EVER)
     {
-      bufferGpsReceive[dataSize] = '\0';
-      const char *pGpsData = bufferGpsReceive;
+        char bufferGpsReceive[BUFFER_SIZE] = {0};
+        uint8_t dataSize = Serial1.readBytesUntil('\n', bufferGpsReceive, BUFFER_SIZE);
 
-      // Test data
-      // Good Received data: $GNGGA,201427.00,4522.65273,N,07155.50339,W,2,12,0.71,282.0,M,-31.5,M,,0000*78
-      // char gpsDataTest[BUFFER_SIZE] = {'$', 'G', 'N', 'G', 'G', 'A', ',', '2', '0', '1', '4', '2', '7', '.', '0', '0', ',', '4', '5', '2', '2', '.', '6', '5', '2', '7', '3', ',', 'N', ',', '0', '7', '1', '5', '5', '.', '5', '0', '3', '3', '9', ',', 'W', ',', '2', ',', '1', '2', ',', '0', '.', '7', '1', ',', '2', '8', '2', '.', '0', ',', 'M', ',', '-', '3', '1', '.', '5', ',', 'M', ',', ',', '0', '0', '0', '0', '*', '7', '8', '\0'};
-      // const char *pGpsDataTest = gpsDataTest;
+        if (dataSize > 0)
+        {
+            bufferGpsReceive[dataSize] = '\0';
+            const char *pGpsData = bufferGpsReceive;
 
-      splitData(pGpsData);
+            // Test data
+            // Good Received data: $GNGGA,201427.00,4522.65273,N,07155.50339,W,2,12,0.71,282.0,M,-31.5,M,,0000*78
+            // char gpsDataTest[BUFFER_SIZE] = {'$', 'G', 'N', 'G', 'G', 'A', ',', '2', '0', '1', '4', '2', '7', '.', '0', '0', ',', '4', '5', '2', '2', '.', '6', '5', '2', '7', '3', ',', 'N', ',', '0', '7', '1', '5', '5', '.', '5', '0', '3', '3', '9', ',', 'W', ',', '2', ',', '1', '2', ',', '0', '.', '7', '1', ',', '2', '8', '2', '.', '0', ',', 'M', ',', '-', '3', '1', '.', '5', ',', 'M', ',', ',', '0', '0', '0', '0', '*', '7', '8', '\0'};
+            // const char *pGpsDataTest = gpsDataTest;
+
+            splitData(pGpsData);
+        }
     }
-  }
 }
 
 void loop() {}
 
 void splitData(const char pGpsData[BUFFER_SIZE])
 {
-  char data[NB_ELEMENTS][SIZE_ELEMENTS] = {0};
-  uint8_t indexElements = 0;
-  uint8_t indexChar = 0;
-  bool valide = true;
+    char data[NB_ELEMENTS][SIZE_ELEMENTS] = {0};
+    uint8_t indexElements = 0;
+    uint8_t indexChar = 0;
+    bool valide = true;
 
-  if (pGpsData[0] == '\n' || pGpsData[0] == '\0' || pGpsData[0] != '$')
-  {
-    valide = false;
-    LOG(WARN, "Empty message or not formatted correctly");
-  }
-  else
-  {
-    for (uint8_t i = 0; pGpsData[i] != '\n' && pGpsData[i] != '\0' && i < BUFFER_SIZE && indexElements + 1 < NB_ELEMENTS && indexChar + 1 < SIZE_ELEMENTS; i++)
+    if (pGpsData[0] == '\n' || pGpsData[0] == '\0' || pGpsData[0] != '$')
     {
-      if (pGpsData[i] == ',')
-      {
-        data[indexElements][indexChar + 1] = '\0';
-        indexChar = 0;
-        indexElements++;
-      }
-      else
-      {
-        data[indexElements][indexChar] = pGpsData[i];
-        indexChar++;
-      }
-    }
-  }
-
-  char compareMsgType[] = "$GNGGA";
-  if (strcmp(data[0], compareMsgType) == 0)
-  {
-    LOG(INFO, "Good format of received data: %s", pGpsData);
-    fixType = (uint8_t)atoi(data[6]);
-    if (fixType == 0u)
-    {
-      valide = false;
-      LOG(WARN, "Invalid signal, no position available");
-    }
-  }
-  else
-  {
-    valide = false;
-  }
-
-  if (valide)
-  {
-    if (data[2][0] != '\0')
-    {
-      latitude = getLatitude(data[2], data[3]);
-      LOG(INFO, "Latitude: %f", latitude);
+        valide = false;
+        LOG(WARN, "Empty message or not formatted correctly");
     }
     else
     {
-      valide = false;
-      LOG(WARN, "Empty messages");
+        for (uint8_t i = 0; pGpsData[i] != '\n' && pGpsData[i] != '\0' && i < BUFFER_SIZE && indexElements + 1 < NB_ELEMENTS && indexChar + 1 < SIZE_ELEMENTS; i++)
+        {
+            if (pGpsData[i] == ',')
+            {
+                data[indexElements][indexChar + 1] = '\0';
+                indexChar = 0;
+                indexElements++;
+            }
+            else
+            {
+                data[indexElements][indexChar] = pGpsData[i];
+                indexChar++;
+            }
+        }
     }
 
-    if (data[4][0] != '\0')
+    char compareMsgType[] = "$GNGGA";
+    if (strcmp(data[0], compareMsgType) == 0)
     {
-      longitude = getLongitude(data[4], data[5]);
-      LOG(INFO, "Longitude: %f", longitude);
+        LOG(INFO, "Good format of received data: %s", pGpsData);
+        g_fixType = (uint8_t)atoi(data[6]);
+        if (g_fixType == 0u)
+        {
+            valide = false;
+            LOG(WARN, "Invalid signal, no position available");
+        }
     }
     else
     {
-      valide = false;
-      LOG(WARN, "Empty messages");
+        valide = false;
     }
-  }
+
+    if (valide)
+    {
+        if (data[2][0] != '\0')
+        {
+            g_latitude = getLatitude(data[2], data[3]);
+            LOG(INFO, "Latitude: %f", g_latitude);
+        }
+        else
+        {
+            valide = false;
+            LOG(WARN, "Empty messages");
+        }
+
+        if (data[4][0] != '\0')
+        {
+            g_longitude = getLongitude(data[4], data[5]);
+            LOG(INFO, "Longitude: %f", g_longitude);
+        }
+        else
+        {
+            valide = false;
+            LOG(WARN, "Empty messages");
+        }
+    }
 }
 
 float getLatitude(char latData[SIZE_ELEMENTS], char latSign[SIZE_ELEMENTS])
 {
-  float allDegrees = 0.0f;
-  float allMinutes = 0.0f;
-  int8_t iDeg = 1;
-  int8_t iMin = 1;
+    float allDegrees = 0.0f;
+    float allMinutes = 0.0f;
+    int8_t iDeg = 1;
+    int8_t iMin = 1;
 
-  for (uint8_t i = 0; latData[i] != '\0' && i < SIZE_ELEMENTS; i++)
-  {
-    uint8_t temp = 0;
-    if (latData[i] != '.' && latData[i] != '-')
+    for (uint8_t i = 0; latData[i] != '\0' && i < SIZE_ELEMENTS; i++)
     {
-      temp = latData[i] - '0';
-      if (iDeg >= 0)
-      {
+        uint8_t temp = 0;
+        if (latData[i] != '.' && latData[i] != '-')
+        {
+            temp = latData[i] - '0';
+            if (iDeg >= 0)
+            {
 
-        allDegrees += temp * pow(10, iDeg);
-        iDeg--;
-      }
-      else if (iDeg == -1)
-      {
-        allMinutes += temp * pow(10, iMin);
-        iMin--;
-      }
+                allDegrees += temp * pow(10, iDeg);
+                iDeg--;
+            }
+            else if (iDeg == -1)
+            {
+                allMinutes += temp * pow(10, iMin);
+                iMin--;
+            }
+        }
     }
-  }
-  float latitude = allDegrees + (allMinutes / 60.0f);
-  if (latSign[0] == 'S')
-  {
-    latitude *= -1.0f;
-  }
-  return latitude;
+    float latitude = allDegrees + (allMinutes / 60.0f);
+    if (latSign[0] == 'S')
+    {
+        latitude *= -1.0f;
+    }
+    return latitude;
 }
 
 float getLongitude(char longData[SIZE_ELEMENTS], char longSign[SIZE_ELEMENTS])
 {
-  float allDegrees = 0.0f;
-  float allMinutes = 0.0f;
-  int8_t iDeg = 2;
-  int8_t iMin = 1;
+    float allDegrees = 0.0f;
+    float allMinutes = 0.0f;
+    int8_t iDeg = 2;
+    int8_t iMin = 1;
 
-  for (uint8_t i = 0; longData[i] != '\0' && i < SIZE_ELEMENTS; i++)
-  {
-    uint8_t temp = 0;
-    if (longData[i] != '.' && longData[i] != '-')
+    for (uint8_t i = 0; longData[i] != '\0' && i < SIZE_ELEMENTS; i++)
     {
-      temp = longData[i] - '0';
-      if (iDeg >= 0)
-      {
+        uint8_t temp = 0;
+        if (longData[i] != '.' && longData[i] != '-')
+        {
+            temp = longData[i] - '0';
+            if (iDeg >= 0)
+            {
 
-        allDegrees += temp * pow(10, iDeg);
-        iDeg--;
-      }
-      else if (iDeg == -1)
-      {
-        allMinutes += temp * pow(10, iMin);
-        iMin--;
-      }
+                allDegrees += temp * pow(10, iDeg);
+                iDeg--;
+            }
+            else if (iDeg == -1)
+            {
+                allMinutes += temp * pow(10, iMin);
+                iMin--;
+            }
+        }
     }
-  }
 
-  float longitude = allDegrees + (allMinutes / 60);
-  if (longSign[0] == 'W')
-  {
-    longitude *= -1.0f;
-  }
-  else if (longSign[0] == 'E')
-  {
-    longitude -= 360.0f;
-  }
-  return longitude;
+    float longitude = allDegrees + (allMinutes / 60);
+    if (longSign[0] == 'W')
+    {
+        longitude *= -1.0f;
+    }
+    else if (longSign[0] == 'E')
+    {
+        longitude -= 360.0f;
+    }
+    return longitude;
+}
+
+void canCB(RoverCanLib::CanBusManager *canBusManager_, const twai_message_t *msg_)
+{
+    switch (msg_->identifier)
+    {
+    case (DEVICE_ID):
+        // if (msg_->data_length_code < 3)
+        // {
+        //     LOG(WARN, "Ill formed msg, dropping");
+        //     canBusManager_->sendErrorCode(RoverCanLib::Constant::eInternalErrorCode::WARNING);
+        //     return;
+        // }
+        // else
+        // {
+        //     canBusManager_->resetWatchDog();
+        //     canBusManager_->sendErrorCode(msgPropCmd.parseMsg(msg_));
+        // }
+        break;
+
+    default:
+        break;
+    }
 }
