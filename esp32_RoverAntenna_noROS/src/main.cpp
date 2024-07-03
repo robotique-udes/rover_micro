072790a1d261f97abd7c7d15cabddb18064125bb
Aude Regnault <rega2002@usherbrooke.ca>
2024-07-02 22:08:44 -0400

    Added function to receive msg
    Can't receive more than one

--- esp32_RoverAntenna_noROS/src/main.cpp
+++ esp32_RoverAntenna_noROS/src/main.cpp
@@ -14,38 +14,67 @@ unsigned int localUdpPort = 1234;  // local port to listen on
 char incomingPacket[255];  // buffer for incoming packets
 const char* replyPacket = "pong";  // a reply string to send back
 
+void recvAbtr();
+
+float led_blink = false;
+
 void setup() {
-  Serial.begin(115200);
+    Serial.begin(115200);
 
-  // Set up the ESP32 as an access point
-  Serial.println("Setting up AP (Access Point)...");
-  WiFi.softAP(ssid, password);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
+    pinMode(LED_BUILTIN, OUTPUT);
 
-  // Print the IP address of the access point
-  IPAddress IP = WiFi.softAPIP();
-  Serial.print("AP IP address: ");
-  Serial.println(IP);
+    // Set up the ESP32 as an access point
+    Serial.println("Setting up AP (Access Point)...");
+    WiFi.softAP(ssid, password);
+    WiFi.softAPConfig(local_ip, gateway, subnet);
 
-  // Start listening for UDP packets
-  udp.begin(localUdpPort);
-  Serial.printf("Now listening at IP %s, UDP port %d\n", IP.toString().c_str(), localUdpPort);
+    // Print the IP address of the access point
+    IPAddress IP = WiFi.softAPIP();
+    Serial.print("AP IP address: ");
+    Serial.println(IP);
+
+    // Start listening for UDP packets
+    udp.begin(localUdpPort);
+    Serial.printf("Now listening at IP %s, UDP port %d\n", IP.toString().c_str(), localUdpPort);
 }
 
 void loop() {
-  int packetSize = udp.parsePacket();
-  if (packetSize) {
-    // Receive incoming UDP packets
-    int len = udp.read(incomingPacket, 255);
-    if (len > 0) {
-      incomingPacket[len] = 0;  // Null-terminate the string
-    }
-    Serial.printf("Received %d bytes: %s\n", len, incomingPacket);
+    int packetSize = udp.parsePacket();
+    Serial.printf("packetSize : %d\n", packetSize);
 
-    // Send a reply to the IP address and port that sent the incoming packet
-    udp.beginPacket(udp.remoteIP(), udp.remotePort());
-    udp.write((const uint8_t*)replyPacket, strlen(replyPacket));
-    udp.endPacket();
-    Serial.printf("Sent %s to %s:%d\n", replyPacket, udp.remoteIP().toString().c_str(), udp.remotePort());
+    if (packetSize) {
+        recvAbtr();
+//   if (packetSize) {
+//     // Receive incoming UDP packets
+//     int len = udp.read(incomingPacket, 255);
+//     if (len > 0) {
+//       incomingPacket[len] = 0;  // Null-terminate the string
+//     }
+//     Serial.printf("Received %d bytes: %s\n", len, incomingPacket);
+
+//     // Send a reply to the IP address and port that sent the incoming packet
+//     udp.beginPacket(udp.remoteIP(), udp.remotePort());
+//     udp.write((const uint8_t*)replyPacket, strlen(replyPacket));
+//     udp.endPacket();
+//     Serial.printf("Sent %s to %s:%d\n", replyPacket, udp.remoteIP().toString().c_str(), udp.remotePort());
   }
+}
+
+void recvAbtr()
+{
+    // int packetSize = udp.parsePacket();
+    // if (packetSize) {
+        int len = udp.read(incomingPacket, 255);
+        if (len > 0) {
+            incomingPacket[len] = 0;  // Null-terminate the string
+        }
+        Serial.printf("Received %d bytes: %s\n", len, incomingPacket);
+        led_blink = led_blink == HIGH ? LOW : HIGH;
+        digitalWrite(LED_BUILTIN, led_blink);
+        // if (incomingPacket == "0.00")
+        // {
+        //     led_blink = led_blink == HIGH ? LOW : HIGH;
+        //     digitalWrite(LED_BUILTIN, led_blink);
+        // }
+    // }
 }
