62d6b9c4fffbb3b310e17e277afa4657dd1b9fdb
Araa2876 <anibal.arango@usherbrooke.ca>
2025-05-06 22:29:54 -0400

    more test for moving_average

--- lib_developpement/test/native/helpers/moving_average.hpp
+++ lib_developpement/test/native/helpers/moving_average.hpp
@@ -3,11 +3,6 @@
 #include "rover_lib2/helpers/moving_average.hpp"
 
 // =============================================================================
-// Helpers
-// =============================================================================
-
-
-// =============================================================================
 // Test Suite
 // =============================================================================
 TEST(SUITE_HELPER_Average, Construction)
@@ -26,15 +21,17 @@ TEST(SUITE_HELPER_Average, Adding_Value)
     ASSERT_EQ(average.getAverage(), static_cast<float>(2));
 }
 
-TEST(SUITE_HELPER_Average, Adding_Value_Multiple_Times)
+TEST(SUITE_HELPER_Average, Full_Average_Table)
 {
     MovingAverage<int8_t, 3> average(0);
 
     average.addValue(1);
     average.addValue(5);
     average.addValue(3);
+
+    average.addValue(10);
     
-    ASSERT_EQ(average.getAverage(), static_cast<float>(3));
+    ASSERT_EQ(average.getAverage(), static_cast<float>(6));
 }
 
 TEST(SUITE_HELPER_Average, Adding_Negative_Value)
@@ -46,4 +43,14 @@ TEST(SUITE_HELPER_Average, Adding_Negative_Value)
     average.addValue(3);
     
     ASSERT_EQ(average.getAverage(), static_cast<float>(1));
+}
+
+TEST(SUITE_HELPER_Average, Singular_Coefficient)
+{
+    MovingAverage<int8_t, 1> average(0);
+
+    average.addValue(10);
+    average.addValue(30);
+
+    ASSERT_EQ(average.getAverage(), static_cast<float>(30));
 }
