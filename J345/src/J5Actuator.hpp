a5346b69ebcfca60b2dfc5ab8d7af3e8ff7c6f1c
pmichaud29 <micp1402@usherbrooke.ca>
2025-07-17 20:35:35 -0400

    added can to j345 controller

--- J345/src/J5Actuator.hpp
+++ J345/src/J5Actuator.hpp
@@ -7,9 +7,20 @@
 #include "rover_lib2/helpers/loop_timer.hpp"
 #include "config.hpp"
 
+#include "rover_can2/rover_can2.hpp"
+#include "rover_can2/msgs/arm_joint_cmd.hpp"
+
 class J5Actuator
 {
     static constexpr uint64_t LOOP_PERIOD_US = 500ULL;
+    static constexpr float MAX_SPEED_RAD_S = 1.0F;
+    static constexpr float MIN_SPEED_RAD_S = -1.0F;
+
+    static constexpr float CAN_SEND_FREQUENCY = 20.0F;
+    static constexpr uint64_t CAN_SEND_PERIOD_MS = static_cast<uint64_t>(ROUND(1'000.0F / CAN_SEND_FREQUENCY));
+
+    using JointCanDeviceT = RoverCan2::Device<RoverCan2::SubscriberMember<RoverCan2::Msgs::ArmJointCmd, J5Actuator>,
+                                              RoverCan2::Publisher<RoverCan2::Msgs::ArmJointStatus>>;
 
   public:
     J5Actuator() = default;
@@ -31,19 +42,53 @@ class J5Actuator
 
         if (_pbOpen.isClicked())
         {
-            _driver.setCmd(100.0F);
+            _driver.setCmd(MotorDrivers::MAX_CMD_OPEN_LOOP);
         }
         else if (_pbClose.isClicked())
         {
-            _driver.setCmd(-100.0F);
+            _driver.setCmd(MotorDrivers::MIN_CMD_OPEN_LOOP);
+        }
+        else if (!IN_ERROR(targetSpeed_, 0.01F, 0.0F))
+        {
+            float cmd = MAP(targetSpeed_,
+                            MIN_SPEED_RAD_S,
+                            MAX_SPEED_RAD_S,
+                            MotorDrivers::MIN_CMD_OPEN_LOOP,
+                            MotorDrivers::MAX_CMD_OPEN_LOOP);
+            cmd = std::clamp(cmd, MotorDrivers::MIN_CMD_OPEN_LOOP, MotorDrivers::MAX_CMD_OPEN_LOOP);
+            _driver.setCmd(cmd);
         }
         else
         {
             _driver.setCmd(0.0F);
         }
+
+        if (_timerCanSend.isReady())
+        {
+            RoverCan2::Msgs::ArmJointStatus armStatusMsg;
+            armStatusMsg.data().currentPosition = 0.0F;  // No position feedback on joint yet
+            armStatusMsg.data().currentSpeed = _driver.getCmd();
+
+            _j5CanDevice.sendMsg(armStatusMsg);
+        }
+    }
+
+    JointCanDeviceT& getUnderlyingCanDevice()
+    {
+        return _j5CanDevice;
     }
 
   private:
+    void CB_canCmd(const RoverCan2::Msgs::ArmJointCmd& cmd_)
+    {
+        targetSpeed_ = cmd_.getData().targetSpeed;
+    }
+
+    JointCanDeviceT _j5CanDevice
+        = JointCanDeviceT(RoverCan2::Constant::eDeviceId::GRIPPER_CLOSE_CONTROLLER,
+                          RoverCan2::SubscriberMember<RoverCan2::Msgs::ArmJointCmd, J5Actuator>(*this, &J5Actuator::CB_canCmd),
+                          RoverCan2::Publisher<RoverCan2::Msgs::ArmJointStatus>());
+
     PWMGenerators::MCPWMTimer __pwmTimer = PWMGenerators::MCPWMTimer(1'000, PWMGenerators::MCPWMTimer::eMCPWMGroupID::GROUP_1);
     PWMGenerators::MCPWM __pwmGen = PWMGenerators::MCPWM(PIN_J5_PWM, __pwmTimer);
     MotorDrivers::IFX9201SG<PWMGenerators::MCPWM> _driver
@@ -53,6 +98,9 @@ class J5Actuator
 
     PushButton _pbOpen = {PIN_PB_J5_OPEN};
     PushButton _pbClose = {PIN_PB_J5_CLOSE};
+
+    float targetSpeed_ = 0.0F;
+    LoopTimer<uint64_t, &Time::micros> _timerCanSend = {CAN_SEND_PERIOD_MS};
 };
 
 #endif  // J5ACTUATOR_HPP
--- J345/src/main.cpp
+++ J345/src/main.cpp
@@ -6,6 +6,7 @@
 #include "J5Actuator.hpp"
 #include "rover_lib2/sensors/push_button.hpp"
 #include "rover_lib2/helpers/loop_timer.hpp"
+#include "rover_can2/rover_can2.hpp"
 
 DEFINE_LOG_NODE(Main, Logger::eNodeState::ON);
 DEFINE_LOG_NODE(MainPlot, Logger::eNodeState::ON);
@@ -15,7 +16,7 @@ void setup(void)
 #if defined(PCB_ROVER_J345_REV1)
     {
         // PWM pin is on a pin with default behavior (TX0) and is high by default, this cause the motor to glitch at startup. This
-        // block limits the impact of this
+        // block helps limits its impact
         IO::DigitalOutput j34L_pwmPin(PIN_J34_R_PWM);
         j34L_pwmPin.write(IO::eIOState::LOW_);
     }
@@ -26,8 +27,8 @@ void setup(void)
     delay(1000);
 #endif
 
-    LED::LedBlinkerSoft led(IO::DigitalOutput(PIN_USER_LED), LED::BlinkPatterns::HEARTBEAT);
-    led.init();
+    LED::LedBlinkerSoft statusLed(IO::DigitalOutput(PIN_USER_LED), LED::BlinkPatterns::HEARTBEAT);
+    statusLed.init();
 
     J34Device j34;
     j34.init();
@@ -35,12 +36,18 @@ void setup(void)
     J5Actuator j5;
     j5.init();
 
+    LED::LedBlinkerSoft canLed(IO::DigitalOutput(PIN_CAN_LED), LED::BlinkPatterns::HEARTBEAT);
+    RoverCan2::Drivers::DriverESP32<LED::LedBlinkerSoft> canDriver(PIN_CAN_RX, PIN_CAN_TX, &canLed);
+    RoverCan2::ManagerSlave canManager(canDriver, j34.getJ3Device(), j34.getJ4Device(), j5.getUnderlyingCanDevice());
+    canManager.init();
+
     LOG_INFO(Logger::Nodes::Main, "J345 Init done, starting main loop!");
     for (EVER)
     {
-        led.update();
+        statusLed.update();
         j34.update();
         j5.update();
+        canManager.update();
     }
 }
 
